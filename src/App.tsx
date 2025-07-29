9a25b674fef3d9d16361db4e51b03e7de222daa3
Guilherme Lima <ghduep@outlook.com>
2025-07-29 07:26:23 -0300

    ultimas mudancas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import './styles/global.css';
 import './styles/theme.css';
 
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { Countdown } from './components/Countdown';
+import { Input } from './components/Input';
+import { Cycles } from './components/Cycles';
 
 function App() {
     return (
         <>
-       <Container><Heading>Pomodoro Timer</Heading></Container>
+       <Container><Logo /></Container>
+
+       <Container><Menu /></Container>
+
+       <Container><Countdown /></Container>
+
+       <Container>
+        <form className="form" action="">
+            <div className="formRow">
+                <Input labelText="task" id="meuInput" type="text" placeholder="Digite sua tarefa" />
+            </div>
+
+            <div className="formRow">
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className="formRow">
+                <Cycles />
+            </div>
+
+            <div className="formRow">
+                <button>Enviar</button>
+            </div>
+        </form>
+       </Container>
         </>
     );
 }
---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export function Countdown() {
+    return (
+        <div className={styles.container}>
+            00:00
+        </div>
+    )
+}
